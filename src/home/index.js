bc2ea46c1b12f42d72de218f0315bcdcfd0040c0
ext.qinjun.sui <ext.qinjun.sui@king.com>
2021-05-21 22:36:37 -0700

    update home page ui

--- src/home/index.js
+++ src/home/index.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Home() {
+    const [url, setUrl] = useState('');
+
+
+    const submitForm = () => {
+        console.log("Successful")
+    }
+
     return (
-        <div>Home</div>
+        <div style={{ display: "flex", flexDirection: "column", alignContent: "center" }}>
+            <h1>Please input the URL</h1>
+            <div>
+                <span>http(s)://</span>
+                <input type="text"
+                    placeholder="type url here"
+                    value={url}
+                    onChange={event => setUrl(event.target.value)}
+                    style={{ height: 30, width: 500, paddingLeft: 12, marginLeft: 8, marginRight: 8 }}
+                />
+                <button onClick={submitForm}>
+                    Submit
+            </button>
+            </div>
+        </div>
     )
 }
